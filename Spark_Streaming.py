1ea0cc962b7f7b072519eb98fd87f62e674bd0db
Adun Ajiye <adunajiye@gmail.com>
2024-01-16 14:46:15 +0100

    env

--- Spark_Streaming.py
+++ Spark_Streaming.py
@@ -27,16 +27,6 @@ def create_sparksession():
                 .config('spark.jars.packages',jars)\
                 .config('spark.master',cluster_manager)\
                 .getOrCreate()
-      # Enable hadoop s3a settings
-    # spark.sparkContext._jsc.hadoopConfiguration().set("com.amazonaws.services.s3.enableV4", "true")
-    # spark.sparkContext._jsc.hadoopConfiguration().set("fs.s3a.impl", "org.apache.hadoop.fs.s3a.S3AFileSystem")
-    # spark.sparkContext._jsc.hadoopConfiguration().set("fs.s3a.aws.credentials.provider", \
-    #                                     "com.amazonaws.auth.InstanceProfileCredentialsProvider,com.amazonaws.auth.DefaultAWSCredentialsProviderChain")
-    # spark.sparkContext._jsc.hadoopConfiguration().set("fs.AbstractFileSystem.s3a.impl", "org.apache.hadoop.fs.s3a.S3A")
-
-    # spark.sparkContext._jsc.hadoopConfiguration().set("fs.s3a.access.key",access_key)
-    # spark.sparkContext._jsc.hadoopConfiguration().set("fs.s3a.secret.key",secret_key)
-    # spark.sparkContext._jsc.hadoopConfiguration().set("fs.s3a.endpoint", "s3.us-east-1.amazonaws.com")
       # Enable GCS settings
     spark.sparkContext._jsc.hadoopConfiguration().set("fs.gs.impl", "com.google.cloud.hadoop.fs.gcs.GoogleHadoopFileSystem")
     spark.sparkContext._jsc.hadoopConfiguration().set("fs.AbstractFileSystem.gs.impl", "com.google.cloud.hadoop.fs.gcs.GoogleHadoopFileSystem")
@@ -92,4 +82,5 @@ def write_stream():
     .option('checkpointLocation',checkpoint_uri)\
     .start().awaitTermination()
     print('Streaming to GCS.......')
-print(write_stream())
+    
+write_stream()
