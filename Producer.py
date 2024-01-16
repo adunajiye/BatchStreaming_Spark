abf830e23c0910e66ecaac6d1be8459423a68198
Adun Ajiye <adunajiye@gmail.com>
2024-01-16 13:24:58 +0100

    Stream_Data

---
+++ Producer.py
@@ -0,0 +1,48 @@
+from faker import Faker
+# from kafka import KafkaProducer
+import simplejson as json
+# from dotenv import dotenv_values
+import random
+from confluent_kafka import SerializingProducer
+import json
+
+fake=Faker()
+credit_no=fake.credit_card_number()
+def delivery_report(err, msg):
+    if err is not None:
+        print(f'Message delivery failed: {err}')
+    else:
+        print(f'Message delivered to {msg.topic()} [{msg.partition()}]')
+# delivery_report()
+
+def Produce_kafka():
+    first_name=fake.first_name()
+    last_name=fake.last_name()
+    address=fake.address()
+    email=fake.email()
+    credit_no=fake.credit_card_number()
+    company=fake.company()
+    quantity=fake.random_digit_not_null_or_empty()
+    price=random.randint(50000,1000000)
+    producer_data={'first_name':first_name,'last_name':last_name,'address':address,'email':email,
+                'credit_no':credit_no,'company':company,'quantity':quantity,'price':price}
+    print(producer_data)
+    kafka_host = "164.92.85.68:9092"
+    # Kafka Topics
+    faker_topic = 'faker_topic'
+    producer = SerializingProducer({'bootstrap.servers': f'{kafka_host}',})
+    # produce_schema = create_fake_schema()
+    # print(produce_schema)
+    for i in range(1000):
+        
+            
+    # Produce a message to a Kafka topic
+            producer.produce(faker_topic,
+                             key=credit_no,
+                             value=json.dumps(producer_data),
+            on_delivery=delivery_report
+        )
+
+            print('Produced data {}, data: {}'.format(i, producer_data))
+            producer.flush()
+Produce_kafka()
---
+++ Spark_Streaming.py
@@ -0,0 +1,85 @@
+import pyspark
+from pyspark.sql import SparkSession
+from kafka import KafkaConsumer
+import json
+# from dotenv import dotenv_values
+import boto3
+from kafka import KafkaConsumer
+from datetime import datetime
+from pyspark.sql.functions import explode,from_json,col
+from pyspark.sql.types import StringType,IntegerType,StructType,StructField
+current_date=str(datetime.now())
+# env_var=dotenv_values('.env')
+access_key=""
+secret_key=""
+
+
+
+cluster_manager="spark://164.92.85.68:7077"
+packages = [ "org.apache.spark:spark-sql-kafka-0-10_2.12:3.5.0"
+                    #  "org.apache.hadoop:hadoop-common:3.3.6",
+                    #  "org.scala-lang:scala-library:2.13.0"
+]
+jars = ",".join(packages)  
+gcs_keyfile = ""
+gcs_keyfilee = gcs_keyfile
+def create_sparksession():
+    spark = SparkSession.builder.appName('Streaming Pipeline')\
+                .config('spark.jars.packages',jars)\
+                .config('spark.master',cluster_manager)\
+                .getOrCreate()
+      # Enable hadoop s3a settings
+    # spark.sparkContext._jsc.hadoopConfiguration().set("com.amazonaws.services.s3.enableV4", "true")
+    # spark.sparkContext._jsc.hadoopConfiguration().set("fs.s3a.impl", "org.apache.hadoop.fs.s3a.S3AFileSystem")
+    # spark.sparkContext._jsc.hadoopConfiguration().set("fs.s3a.aws.credentials.provider", \
+    #                                     "com.amazonaws.auth.InstanceProfileCredentialsProvider,com.amazonaws.auth.DefaultAWSCredentialsProviderChain")
+    # spark.sparkContext._jsc.hadoopConfiguration().set("fs.AbstractFileSystem.s3a.impl", "org.apache.hadoop.fs.s3a.S3A")
+
+    # spark.sparkContext._jsc.hadoopConfiguration().set("fs.s3a.access.key",access_key)
+    # spark.sparkContext._jsc.hadoopConfiguration().set("fs.s3a.secret.key",secret_key)
+    # spark.sparkContext._jsc.hadoopConfiguration().set("fs.s3a.endpoint", "s3.us-east-1.amazonaws.com")
+      # Enable GCS settings
+    spark.sparkContext._jsc.hadoopConfiguration().set("fs.gs.impl", "com.google.cloud.hadoop.fs.gcs.GoogleHadoopFileSystem")
+    spark.sparkContext._jsc.hadoopConfiguration().set("fs.AbstractFileSystem.gs.impl", "com.google.cloud.hadoop.fs.gcs.GoogleHadoopFileSystem")
+
+    # Set GCS credentials
+    spark.sparkContext._jsc.hadoopConfiguration().set("google.cloud.auth.service.account.json.keyfile",gcs_keyfilee)
+    return spark   
+# create_sparksession()
+
+
+
+def schema():
+    columns = StructType([StructField('first_name',
+    StringType(), False),
+    StructField('last_name', StringType(), False),
+    StructField('address', StringType(), False),
+    StructField('email', StringType(), False),
+    StructField('credit_no', IntegerType(), False),
+    StructField('company', StringType(), False),
+    StructField('quantity', IntegerType(), False),
+    StructField('price',IntegerType(), False)])
+    print(columns)
+    return columns
+# schema()
+
+
+def read_stream():
+    kafka_boostrap_server = "164.92.85.68" + ":9092"
+    print(kafka_boostrap_server)
+    kafka_topic = "voters_topic"
+    print(kafka_topic)
+    
+    stream=create_sparksession()
+    # print(stream)
+    df1 =stream.readStream \
+    .format("kafka") \
+    .option("kafka.bootstrap.servers", kafka_boostrap_server) \
+    .option("subscribe", kafka_topic) \
+    .option("startingOffsets", "earliest") \
+    .option('includeHeaders','true')\
+    .load()
+    df2=df1.selectExpr("CAST(value AS STRING)",'headers')
+    df3=df2.select(from_json('value',schema=schema()).alias('temp')).select('temp.*')
+    return df3
+read_stream()
